8830ea7311f6d8ba58a4561281711a6462efa37e
jonathanmcdonagh <jonathan.a.mcdonagh@gmail.com>
2020-01-26 12:19:34 +0000

    Initial commit

---
+++ routes/jobs.js
@@ -0,0 +1,106 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+let uriUtil = require('mongodb-uri');
+let Job = require('../models/jobs');
+let Fuse = require('fuse.js');
+
+var mongodbUri = 'mongodb+srv://jonathanmcdonagh:20074520@web-app-cluster-uct5k.mongodb.net/delivermedb?retryWrites=true&w=majority';
+
+
+// noinspection JSIgnoredPromiseFromCall
+mongoose.connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+let db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('Unable to connect to [ ' + db.name + ' ]', err);
+});
+db.once('open', function () {
+    console.log('Successfully connected to DeliverMe Database as ' + db.name);
+});
+
+
+//Find all
+router.findAll = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    Job.find(function(err, jobs) {
+        if (err)
+            res.send(err);
+        else
+            res.send(jobs,null,5);
+    });
+};
+
+
+//Find one by ID
+router.findById = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    Job.find({ "_id" : req.params.id },function(err, job) {
+        if (err)
+            res.json({ message: 'Job NOT Found!', errmsg : err } );
+        else
+            res.send(job,null,5);
+    });
+};
+
+//Add an job
+router.addJob = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    var job = new Job();
+    job.name = req.body.name;
+    job.deliveryRequest = req.body.deliveryRequest;
+    job.place = req.body.place;
+    job.deliveryFee = req.body.deliveryFee;
+    job.dropOffLocation = req.body.dropOffLocation;
+    job.dropOffTime = req.body.dropOffTime;
+
+    job.save(function(err) {
+        if (err)
+            res.json({ message: 'Job NOT Added!', errmsg : err } );
+        else
+            res.json({ message: 'Job Successfully Added!', data: job });
+    });
+};
+
+
+//Updates job
+router.updateJob = (req, res) => {
+
+    Job.findById(req.params.id, function(err,job) {
+        if (err)
+            res.json({ message: 'Job NOT Found!', errmsg : err } );
+        else {
+            job.name = req.body.name;
+            job.deliveryRequest = req.body.deliveryRequest;
+            job.place = req.body.place;
+            job.deliveryFee = req.body.deliveryFee;
+            job.dropOffLocation = req.body.dropOffLocation;
+            job.dropOffTime = req.body.dropOffTime;
+
+            job.save(function (err) {
+                if (err)
+                    res.json({ message: 'Job NOT updated!', errmsg : err } );
+                else
+                    res.json({ message: 'Job Successfully updated!', data: job });
+            });
+        }
+    });
+};
+
+
+//Deletes Job
+router.deleteJob = (req, res) => {
+
+    Job.findByIdAndRemove(req.params.id, function(err) {
+        if (err)
+            res.json({ message: 'Job NOT DELETED!', errmsg : err } );
+        else
+            res.json({ message: 'Job Successfully Deleted!'});
+    });
+};
+
+
+module.exports = router;
