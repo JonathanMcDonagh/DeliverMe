fbf4a61ce298d5daf7358a3019a42f298d6136de
jonathanmcdonagh <jonathan.a.mcdonagh@gmail.com>
2020-03-02 12:53:03 +0000

    Fixing Heroku Issue

--- models/jobs.js
+++ models/jobs.js
@@ -1,14 +1,13 @@
 let mongoose = require('mongoose');
 
 let DeliverMeSchema = new mongoose.Schema({
-        name : String,
-        deliveryRequest : String,
-        place : String,
-        deliveryFee : String,
-        dropOffLocation : String,
-        dropOffTime: String,
+            name : String,
+            deliveryRequest : String,
+            place : String,
+            deliveryFee : String,
+            dropOffLocation : String,
+            dropOffTime: String,
     },
     { collection: 'jobs' });
 
 module.exports = mongoose.model('Job', DeliverMeSchema);
-
